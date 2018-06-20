46fc5afeb29d43c585b142ab924b0f2fbe38cdaa
David <3614449+GenesisFR@users.noreply.github.com>
2018-06-20 04:20:06 -0400

    + added support for chapters * fixed a crash when pressing the Unlock button while the registry key was missing - minor code refactoring

--- TheWitcher2SavelockBypasser/TheWitcher2SavelockBypasserForm.cs
+++ TheWitcher2SavelockBypasser/TheWitcher2SavelockBypasserForm.cs
@@ -6,12 +6,17 @@ using System.Windows.Forms;
 
 namespace TheWitcher2SavelockBypasser
 {
+    // TODO: add support for Epilogue
     public partial class TheWitcher2SavelockBypasserForm : Form
     {
-        private RegistryChangeMonitor regMonitor;
+        private RegistryChangeMonitor regMonitor; // tracks changes made to the registry key below
         private const string keyName = @"HKEY_CURRENT_USER\Software\CD Projekt RED\The Witcher 2";
+        private const string formattedKeyName = @"HKCU\Software\CD Projekt RED\The Witcher 2\GameData";
         private object value;
         private byte[] bytes;
+        private bool isInPrologue;
+        private bool isInChapters;
+        private bool isInEpilogue;
         private bool isUnlocked;
 
         private delegate void refreshDelegate();
@@ -20,7 +25,7 @@ namespace TheWitcher2SavelockBypasser
         public TheWitcher2SavelockBypasserForm()
         {
             InitializeComponent();
-            RegisterEvents();
+            StartMonitoringRegistry();
 
             refreshLabelDelegate = new refreshDelegate(refreshLabel);
             queryRegistry();
@@ -33,15 +38,22 @@ namespace TheWitcher2SavelockBypasser
 
         private void TheWitcher2SavelockBypasserForm_FormClosed(object sender, FormClosedEventArgs e)
         {
-            // Stop listening for events
-            regMonitor.Dispose();
+            StopMonitoringRegistry();
         }
 
-        private void RegisterEvents()
+        private void StartMonitoringRegistry()
         {
             regMonitor = new RegistryChangeMonitor(keyName, RegistryChangeMonitor.REG_NOTIFY_CHANGE.LAST_SET);
             regMonitor.Changed += OnRegistryChanged;
+
+            // Start listening for events
             regMonitor.Start();
+        }
+
+        private void StopMonitoringRegistry()
+        {
+            // Stop listening for events
+            regMonitor.Dispose();
         }
 
         private void OnRegistryChanged(object sender, RegistryChangeEventArgs e)
@@ -52,7 +64,10 @@ namespace TheWitcher2SavelockBypasser
 
         private void queryRegistry()
         {
-            isUnlocked = false;
+            isUnlocked   = false;
+            isInPrologue = false;
+            isInChapters = false;
+            isInEpilogue = false;
 
             try
             {
@@ -64,11 +79,24 @@ namespace TheWitcher2SavelockBypasser
                     // Convert data to bytes
                     bytes = (byte[])value;
 
-                    // When starting a new insane playthrough, the game will set the 9th byte to 53, which means the save is unlocked
-                    // As soon as you die, this byte will change to 52 and the save will be locked
-                    // It's also locked if the 9th byte is anything other than 53 or if the GameData registry key doesn't exist
-                    if (bytes.Length > 8)
+                    // When playing on insane, the game will set a specific byte to a specific value, based on your progression
+                    // The array of bytes will have a specific length, also based on your progression
+                    isInPrologue = bytes.Length == 25;
+                    isInChapters = bytes.Length == 30;
+                    isInEpilogue = bytes.Length == 35;
+
+                    // As soon as you die, the byte is decremented by one and the save is locked
+                    // It's also locked if the byte is anything other than what it's supposed to be or if the registry key doesn't exist
+
+                    // Prologue: if the 9th byte is 53, the save is unlocked
+                    if (isInPrologue)
                         isUnlocked = bytes[8] == 53;
+                    // Chapters: if the 14th byte is 112, the save is unlocked
+                    else if (isInChapters)
+                        isUnlocked = bytes[13] == 112;
+                    // Epilogue: if the ??th byte is ???, the save is unlocked
+                    else if (isInEpilogue)
+                        isUnlocked = bytes[18] == bytes[18];
                 }
             }
             catch
@@ -96,20 +124,23 @@ namespace TheWitcher2SavelockBypasser
                 // Make sure we're not changing old values
                 queryRegistry();
 
-                if (bytes.Length > 8)
+                if (value != null && bytes != null && bytes.Length > 8)
                 {
-                    // Magic number (yer a wizard, Harry)
-                    bytes[8] = 53;
+                    // Magic numbers (yer a wizard, Harry)
+                    if (isInPrologue)
+                        bytes[8] = 53;
+                    else if (isInChapters)
+                        bytes[13] = 113;
+                    else if (isInEpilogue)
+                        bytes[18] = bytes[18];
 
                     // Update registry
                     Registry.SetValue(keyName, "GameData", bytes, RegistryValueKind.None);
                 }
 
                 bool isKeyFound = value != null;
-                string msg = isKeyFound ? "Save unlocked! Do you want to run the game?" :
-                    @"The following registry key doesn't exist:\n
-                    HKEY_CURRENT_USER\Software\CD Projekt RED\The Witcher 2\GameData\n
-                    doesn't exist. Do you want to run the game and start a new playthrough?";
+                string msg = isKeyFound ? "Save unlocked! Do you want to run the game?" : string.Format("{0}\n\n{1}\n\n{2}",
+                "The following registry key doesn't exist:", formattedKeyName, "Do you want to start a new playthrough?");
 
                 // Offer the user to run the game
                 if (MessageBox.Show(msg, isKeyFound ? "Success" : "Error", MessageBoxButtons.YesNo,
